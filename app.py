1ced7d24f83b5593f6254bc666b9e80283514aaf
markwilde33 <markwilde29@gmail.com>
2018-09-09 18:44:46 +0100

    Created the route/function for deleting tasks by clicking the done button. Linked the route url to the tasks page. tweaked the column layout on tasks page so that the collapsible header lines up better with the buttons

--- app.py
+++ app.py
@@ -49,7 +49,12 @@ def update_task(task_id):
             'is_urgent': request.form.get('is_urgent'),
         })
     return redirect(url_for('get_tasks'))
-    # <! -- forgot to make a comment in git commit for this new route/function -->
+
+
+@app.route('/delete_task/<task_id>')
+def delete_task(task_id):
+    mongo.db.tasks.remove({'_id': ObjectId(task_id)})
+    return redirect(url_for('get_tasks'))
 
 
 if __name__ == '__main__':
