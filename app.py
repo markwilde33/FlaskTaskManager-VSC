bcbced123e58650ae0a6a96f993e445144ecc4ef
markwilde33 <markwilde29@gmail.com>
2018-09-11 20:44:25 +0100

    Created an add categories page.Wrote the route/function and wired it up. Everything working as expected

--- app.py
+++ app.py
@@ -63,12 +63,6 @@ def get_categories():
                            categories=mongo.db.categories.find())
 
 
-@app.route('/delete_category/<category_id>')
-def delete_category(category_id):
-    mongo.db.categories.remove({'_id': ObjectId(category_id)})
-    return redirect(url_for('get_categories'))
-
-
 @app.route('/edit_category/<category_id>')
 def edit_category(category_id):
     return render_template('editcategory.html',
@@ -81,6 +75,24 @@ def update_category(category_id):
         {'_id': ObjectId(category_id)},
         {'category_name': request.form['category_name']})
     return redirect(url_for('get_categories'))
+
+
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    category_doc = {'category_name': request.form.get('category_name')}
+    mongo.db.categories.insert_one(category_doc)
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/add_category')
+def add_category():
+    return render_template('addcategory.html')
 
 
 if __name__ == '__main__':
